031faae0dcc58bf6bc4717264bd12e973f4c8ac4
Filipe Scaglia <filipecscaglia@gmail.com>
2019-12-30 12:18:21 -0300

    user insert function created

---
+++ controllers/UserController.js
@@ -0,0 +1,51 @@
+class UserController{
+
+    constructor(formId, tableId){
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+        this.onSubmit();
+    }
+
+    onSubmit(){
+        this.formEl.addEventListener("submit", e=>{
+            e.preventDefault();
+
+            let user = this.getValues();
+
+            this.addLine(user)
+        });
+    }
+
+    getValues(){
+
+        let user = {};
+
+        [...this.formEl.elements].forEach(f=>{
+            if(f.name == "gender"){
+                if(f.checked){
+                    user[f.name] = f.value;
+                }
+            } else {
+                user[f.name] = f.value;
+            }
+        });
+    
+        return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+    }
+
+    addLine(dataUser) {
+        this.tableEl.innerHTML = `
+            <tr>
+                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${dataUser.admin}</td>
+                <td>${dataUser.birth}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr>
+        `;
+    }
+}
