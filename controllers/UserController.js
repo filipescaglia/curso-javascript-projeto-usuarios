1a755cf68f81f2f6cf65c8923acf465d42b4bfd5
Filipe Scaglia <filipecscaglia@gmail.com>
2020-01-02 14:41:56 -0300

    Edit function created

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,75 @@
 class UserController{
 
-    constructor(formId, tableId){
-        this.formEl = document.getElementById(formId);
+    constructor(formIdCreate, formIdUpdate, tableId){
+        this.formEl = document.getElementById(formIdCreate);
+        this.formUpdateEl = document.getElementById(formIdUpdate);
         this.tableEl = document.getElementById(tableId);
         this.onSubmit();
+        this.onEdit();
+    }
+
+    onEdit(){
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=>{
+            this.showPanelCreate();
+        });
+
+        this.formUpdateEl.addEventListener("submit", e=>{
+            e.preventDefault();
+
+            let btn = this.formUpdateEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
+            let values = this.getValues(this.formUpdateEl);
+
+            console.log(values);
+
+            let index = this.formUpdateEl.dataset.trIndex;
+
+            let tr = this.tableEl.rows[index];
+
+            let userOld = JSON.parse(tr.dataset.user);
+
+            let result = Object.assign({}, userOld, values);
+
+            this.getPhoto(this.formUpdateEl).then(content=>{
+
+                if(!values.photo){
+                    result._photo = userOld._photo;
+                } else{
+                    result._photo = content;
+                }
+
+                tr.dataset.user = JSON.stringify(result);
+
+                tr.innerHTML = `
+                    <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
+                    <td>${result._name}</td>
+                    <td>${result._email}</td>
+                    <td>${(result._admin) ? 'Sim' : 'Não'}</td>
+                    <td>${Utils.dateFormat(result._register)}</td>
+                    <td>
+                        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+                        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    </td>
+                `;
+
+                this.addEventsTr(tr);
+
+                this.updateCount();
+
+                this.formUpdateEl.reset();
+
+                this.formEl.reset();
+
+                btn.disabled = false;
+
+                this.showPanelCreate();
+
+            }, e=>{
+                console.error(e);
+            });
+        });
     }
 
     onSubmit(){
@@ -14,11 +80,11 @@ class UserController{
 
             btn.disabled = true;
 
-            let values = this.getValues();
+            let values = this.getValues(this.formEl);
 
             if(!values) return false;
 
-            this.getPhoto().then(content=>{
+            this.getPhoto(this.formEl).then(content=>{
                 values.photo = content;
 
                 this.addLine(values);
@@ -33,11 +99,11 @@ class UserController{
         });
     }
 
-    getPhoto(){
+    getPhoto(formEl){
         return new Promise((resolve, reject)=>{
             let fileReader = new FileReader();
 
-            let elements = [...this.formEl.elements].filter(item=>{
+            let elements = [...formEl.elements].filter(item=>{
                 if (item.name === 'photo'){
                     return item;
                 }
@@ -61,12 +127,12 @@ class UserController{
         });        
     }
 
-    getValues(){
+    getValues(formEl){
 
         let user = {};
         let isValid = true;
 
-        [...this.formEl.elements].forEach(f=>{
+        [...formEl.elements].forEach(f=>{
 
             if (['name', 'email', 'password'].indexOf(f.name) > -1 && !f.value){
                 f.parentElement.classList.add('has-error');
@@ -103,14 +169,60 @@ class UserController{
             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         `;
 
+        this.addEventsTr(tr);
+
         this.tableEl.appendChild(tr);
 
         this.updateCount();
+    }
+
+    addEventsTr(tr){
+        tr.querySelector(".btn-edit").addEventListener("click", e=>{
+            let json = JSON.parse(tr.dataset.user);
+
+            this.formUpdateEl.dataset.trIndex = tr.sectionRowIndex;
+
+            for(let name in json){
+                let field = this.formUpdateEl.querySelector("[name=" + name.replace("_","") + "]");
+
+                if(field){
+                    
+                    switch (field.type){
+                        case 'file':
+                            continue;
+                            break;
+                        case 'radio':
+                            field = this.formUpdateEl.querySelector("[name=" + name.replace("_","") + "][value="+ json[name] +"]");
+                            field.checked = true;
+                            break;
+                        case 'checkbox':
+                            field.checked = json[name];
+                            break;
+                        default:
+                            field.value = json[name];
+                    }
+                }
+            }
+
+            this.formUpdateEl.querySelector(".photo").src = json._photo;
+
+            this.showPanelUpdate();
+        });
+    }
+
+    showPanelCreate(){
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+    }
+
+    showPanelUpdate(){
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
     }
 
     updateCount(){
